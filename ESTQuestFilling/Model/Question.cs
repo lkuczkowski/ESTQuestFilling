897539c9089fb58907db33d3e07aa01052110916
Łukasz Kuczkowski <lkuczkowski@wp.eu>
2020-08-11 21:00:25 +0200

    Add comment handling for GetSuwak_odczytWartosciCode
    Comment allow to fill intervals and ranges for marks. Comment must be in below
    structure:
        * [ZAKRES] <min, max>; <min, max> Mark; ...
    Min value is also default value (starting point to slider). Marks can be A, W,
    N and are translated by internal dictionary to XML definition. Any invalid
    comment throws exception and default XML Code is assigned. User is informed
    by popup window.

--- ESTQuestFilling/Model/Question.cs
+++ ESTQuestFilling/Model/Question.cs
@@ -52,6 +52,7 @@ namespace ESTQuestFilling.Model
             QuestionText = question;
         }
 
+        // zmienić marks na coś w styli analytics
         public Question(int id, string question, string marks)
             : this(id, question)
         {
@@ -342,20 +343,86 @@ namespace ESTQuestFilling.Model
         // TODO - komentarze
         private string GetSuwak_odczytWartosciCode()
         {
-            return "<!--___________________WPROWADŹ WARTOŚCI: MIN, MAX, DEFAULT________________-->\n" +
-                    "<InputSliderInt minValue=\"\" maxValue=\"\" defaultValue=\"\" required=\"true\">\n" +
-                            $"\t<Title>{QuestionText}</Title>\n" +
-                            "\t<Mark>\n" +
-                                _analyticsLink +
-                                "\t\t<Definition initialMark = \"warning\" refusalMark = \"alarm\">\n" +
-                                    "\t\t\t<!--___________________UZUPEŁNIJ PRZEDZIAŁY________________-->\n" +
-                                    "\t\t\t<MarkDef rangeMin = \"\" rangeMax = \"\" mark = \"alarm\"/>\n" +
-                                    "\t\t\t<MarkDef rangeMin = \"\" rangeMax = \"\" mark = \"warning\"/>\n" +
-                                    "\t\t\t<MarkDef rangeMin = \"\" rangeMax = \"\" mark = \"normal\"/>\n" +
-                                "\t\t</Definition>\n" +
-                            "\t</Mark>\n" +
-                            "\t<NotEdited/>\n" +
-                       "</InputSliderInt>";
+            string marksDefinitionXmlCode = "";
+            string defaultMarksDefinitionXmlCode =
+                "\t\t\t<!--___________________UZUPEŁNIJ PRZEDZIAŁY________________-->\n" +
+                "\t\t\t<MarkDef rangeMin = \"\" rangeMax = \"\" mark = \"alarm\"/>\n" +
+                "\t\t\t<MarkDef rangeMin = \"\" rangeMax = \"\" mark = \"warning\"/>\n" +
+                "\t\t\t<MarkDef rangeMin = \"\" rangeMax = \"\" mark = \"normal\"/>\n";
+            string firstTwolinesOfXmlCode = "";
+            string defaultFirstTwoLinesOfXmlCode =
+                "<!--___________________WPROWADŹ WARTOŚCI: MIN, MAX, DEFAULT________________-->\n" +
+                "<InputSliderInt minValue=\"\" maxValue=\"\" defaultValue=\"\" required=\"true\">\n";
+
+            if (Comment.StartsWith("[ZAKRES]"))
+            {
+                try
+                {
+                    var removedCommentTypeAndSplitToRanges = Comment.Substring(Comment.IndexOf(']') + 1).Trim()
+                        .Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries).Select(n => n.Trim()).ToArray();
+                    if (removedCommentTypeAndSplitToRanges.Length < 2)
+                        throw new ArgumentException("No values to convert after [...] prefix.");
+
+                    var interval = removedCommentTypeAndSplitToRanges.First().Split(new char[] {'<', '>', ',', ' '},
+                        StringSplitOptions.RemoveEmptyEntries);
+                    if (!interval.All(n => int.TryParse(n, out _)) || interval.Length !=2)
+                        throw new ArgumentException("Interval value is NaN or not a interval definition\n");
+
+                    firstTwolinesOfXmlCode =
+                        $"<InputSliderInt minValue=\"{interval[0]}\" maxValue=\"{interval[1]}\" defaultValue=\"{interval[0]}\" required=\"true\">\n";
+
+                    foreach (var rangeAndMark in removedCommentTypeAndSplitToRanges.Skip(1))
+                    {
+                        var rangeAndMarkArray = rangeAndMark
+                            .Split(new char[] {'<', '>', ',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+                        if (rangeAndMarkArray.Length != 3)
+                            throw new ArgumentException("Comment separators error.");
+
+                        if (!rangeAndMarkArray.Take(2).All(n => int.TryParse(n, out _)))
+                        {
+                            throw new ArgumentException("Range value is NaN\n");
+                        }
+
+                        marksDefinitionXmlCode +=
+                            $"\t\t\t<MarkDef rangeMin = \"{rangeAndMarkArray[0]}\" rangeMax = \"{rangeAndMarkArray[1]}\" " +
+                            $"mark = \"{_marksAbbreviationsToXmlNamesDictionary[rangeAndMarkArray[2]]}\"/>\n";
+                    }
+                }
+                catch (KeyNotFoundException e)
+                {
+                    firstTwolinesOfXmlCode = defaultMarksDefinitionXmlCode;
+                    marksDefinitionXmlCode = defaultMarksDefinitionXmlCode;
+
+                    MessageBox.Show(e.Message + "\n\n" +
+                                    "Unable to convert comment due to mark abbreviation error.\n" +
+                                    $"Default marks and remainder assigned to question ID: {this.Id}");
+                }
+                catch (Exception e)
+                {
+                    firstTwolinesOfXmlCode = defaultFirstTwoLinesOfXmlCode;
+                    marksDefinitionXmlCode = defaultMarksDefinitionXmlCode;
+
+                    MessageBox.Show(e.Message + "\n\n" +
+                                    $"Unable to convert comment. Default marks and remainder assigned to question ID: {this.Id}");
+                }
+            }
+            else
+            {
+                firstTwolinesOfXmlCode = defaultFirstTwoLinesOfXmlCode;
+                marksDefinitionXmlCode = defaultMarksDefinitionXmlCode;
+            }
+
+
+            return firstTwolinesOfXmlCode +
+                    $"\t<Title>{QuestionText}</Title>\n" +
+                    "\t<Mark>\n" +
+                        _analyticsLink +
+                        "\t\t<Definition initialMark = \"warning\" refusalMark = \"alarm\">\n" +
+                            marksDefinitionXmlCode +
+                        "\t\t</Definition>\n" +
+                    "\t</Mark>\n" +
+                    "\t<NotEdited/>\n" +
+                "</InputSliderInt>";
         }
 
         private string GetTAKnieTekstCode()
