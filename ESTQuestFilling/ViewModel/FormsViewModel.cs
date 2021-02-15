0642ebe4e9317e39befe308ffdcd709bcde52628
lkuczkowski <lkuczkowski@wp.eu>
2021-02-15 17:29:44 +0100

    Add FormsView Page and its ViewModel
    FormsView is responsible for company selection (new transparent Combo Box) and
    question presentation. It consists of upper bar (Combo Box and search Text Box)
    and lower part (displaying Company View - list of all checkpoints and
    questions. Corresponding View Model added.
    
    Changes in MainWindow commands names.

---
+++ ESTQuestFilling/ViewModel/FormsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESTQuestFilling.ViewModel
+{
+    class FormsViewModel : PageViewModelBase
+    {
+        public FormsViewModel()
+        {
+            CompanyListCollection = new ObservableCollection<string>()
+            {
+                "Empty list"
+            };
+            CurrentInstitutionName = CompanyListCollection[0];
+            CreateCompanyViewModelFromNameCommand = new DelegateCommand(CreateCompanyViewModelFromName);
+        }
+
+        public FormsViewModel(IEnumerable<string> companyList) : this()
+        {
+            CompanyListCollection = new ObservableCollection<string>(_companyListCollection.Concat(companyList));
+        }
+
+        public DelegateCommand CreateCompanyViewModelFromNameCommand { get; }
+
+        private ObservableCollection<string> _companyListCollection;
+        public ObservableCollection<string> CompanyListCollection
+        {
+            get => _companyListCollection;
+            set
+            {
+                _companyListCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void WriteCurrentCompanyFormsToXml()
+        {
+            CurrentCompanyViewModel.WriteCheckpointsToFiles();
+        }
+
+        public string CurrentInstitutionName { get; set; }
+
+        private void CreateCompanyViewModelFromName(object parameter)
+        {
+            //CurrentCompanyViewModel = new CompanyViewModel(parameter.ToString(), DatabasePath);
+        }
+        
+        private CompanyViewModel _currentCompanyViewModel;
+        public CompanyViewModel CurrentCompanyViewModel
+        {
+            get => _currentCompanyViewModel;
+            set
+            {
+                _currentCompanyViewModel = value;
+                CurrentInstitutionName = value.Name;
+                OnPropertyChanged(nameof(CurrentCompanyViewModel));
+                OnPropertyChanged(nameof(CurrentInstitutionName));
+            }
+        }
+    }
+}
